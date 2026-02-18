2e18c701a49fb4c9b0606a4dc6cd2d1b5cdb7bba
Yamisan0 <idris.messaad@gmail.com>
2026-02-18 11:58:38 +0100

    add: scroll, cursor, newline support + OSDev refs
    - Add hardware cursor (blinking) via VGA I/O ports 0x3D4/0x3D5
    - Add scroll when screen is full
    - Add \n support in printk
    - Add concise comments with OSDev wiki references
    - Rebuild ISO

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,28 +1,28 @@
-; Architecture i386
-; NASM syntax
+BITS 32                         ; Mode 32 bits (GRUB nous y a mis)
 
-BITS 32
-
+; --- Header Multiboot (cf. https://wiki.osdev.org/Multiboot) ---
 SECTION .multiboot
 align 4
-    dd 0x1BADB002        ; magic number multiboot
-    dd 0x00              ; flags
-    dd -(0x1BADB002)     ; checksum
+    dd 0x1BADB002               ; Magic number multiboot
+    dd 0x00                     ; Flags
+    dd -(0x1BADB002)            ; Checksum (magic + flags + checksum = 0)
 
+; --- Code executable ---
 SECTION .text
-global start
-extern kernel_main
+global start                    ; Visible par le linker (point d'entree)
+extern kernel_main              ; Definie dans kernel.c
 
 start:
-    cli                  ; disable interrupts
-    mov esp, stack_top   ; setup stack
-    call kernel_main
+    cli                         ; Desactive les interruptions (pas encore de gestionnaire)
+    mov esp, stack_top          ; Configure la pile (necessaire pour appeler des fonctions)
+    call kernel_main            ; Passe la main au C
 .hang:
-    hlt                  ; stop CPU
-    jmp .hang            ; loop if interrupt wakes CPU
+    hlt                         ; CPU en veille
+    jmp .hang                   ; Boucle de securite si reveil inattendu
 
+; --- Pile : 16 Ko, grandit vers le bas ---
 SECTION .bss
 align 16
 stack_bottom:
-    resb 16384           ; 16 KB stack
+    resb 16384
 stack_top:
